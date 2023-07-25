0c8407952f99eaa7930f5240f73a36d65e4b66fa
Sina Norooznia <sina.norooznia@gmail.com>
2023-07-25 20:34:35 +0330

    [feat] coin particle

--- Assets/_Scripts/Controller/Items/CollectibleResource.cs
+++ Assets/_Scripts/Controller/Items/CollectibleResource.cs
@@ -1,6 +1,7 @@
 ﻿using _Scripts.Controller.General;
 using _Scripts.Controller.UI;
 using _Scripts.Models.General;
+using Unity.Mathematics;
 
 namespace _Scripts.Controller.Items
 {
@@ -10,6 +11,7 @@ namespace _Scripts.Controller.Items
         {
             var amount = GameManager.Instance.x2PowerUpActive ? 2 : 1;
             GameManager.Instance.AddResource(amount, ResourceType.Coin);
+            GameHub.Instance.ParticleData.SpawnParticle("coinCollect", transform.position, quaternion.identity);
             base.Interact();
         }
     }
